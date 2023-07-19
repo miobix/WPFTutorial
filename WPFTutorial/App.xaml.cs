ed5056ebfd93da4f972288ccde854be06441577d
miobix <c.aravena90@gmail.com>
2023-07-19 18:05:46 +0900

    Add project files.

---
+++ WPFTutorial/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFTutorial
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //deals with various formatting depending on the locale -- better to do whis when starting the application
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("ko-KR");
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("ko-KR");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+        }
+
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
+    }
+}
---
+++ WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window   //window class is the root of a window, provides min, max, close buttons
+        //"partial" means it needs to be combined with the xaml file
+        //this is done with InitializeComponent() call
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void pnlMainGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("You clicked me at " + e.GetPosition(this).ToString());
+        }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            //accessing resources from the code behind - call to Name.FindResource(resourceName)
+            Console.WriteLine(pnlMain.FindResource("strPanel").ToString());
+            Console.WriteLine(this.FindResource("strWindow").ToString());
+            Console.WriteLine(Application.Current.FindResource("strApp").ToString());
+        }
+    }
+}
